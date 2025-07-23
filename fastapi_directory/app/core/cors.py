5a5fb13fa8b4e2ab82f01b91062cb0faf7aa04d0
Goga <risk-18@mail.ru>
2025-07-24 02:05:22 +0300

    nginx - осталось настроить

--- fastapi_directory/app/core/cors.py
+++ fastapi_directory/app/core/cors.py
@@ -4,7 +4,11 @@ def setup_cors(app):
     # Allow only frontend origin for CORS
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:3000"],
+        allow_origins=[
+            "http://localhost:3000",
+            "http://192.168.1.33",
+            "http://SiebelError.ru"
+        ],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
